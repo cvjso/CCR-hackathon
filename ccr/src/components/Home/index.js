5bd120461d905eba217c1541935e78c4dd598a56
magno-morais <carlosviniciusjose@hotmail.com>
2021-01-16 18:34:02 -0300

    adicionei o router

---
+++ ccr/src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+	render() {
+		return (
+			<div>
+				<text>Banana</text>
+			</div>
+		);
+	}
+}
+export default Home;
