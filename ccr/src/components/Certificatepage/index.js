b76052cddd8dbe5ca7f295fbe9982afddf2627e1
actiiasluna <cvjso@cesar.school>
2021-01-17 20:16:13 -0300

    atualizaçao da pagina de certificados

--- ccr/src/components/Certificatepage/index.js
+++ ccr/src/components/Certificatepage/index.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import certificate1 from '../../images/Rectangle 35.png';
 import certificate2 from '../../images/Rectangle 36.png';
 import Navbar from '../Navbar';
-
+import Panel from '../Panel';
 import './styles.css';
 export class Certificatepage extends Component {
 	render() {
 		return (
 			<div className="main-certicatepage">
+				<Panel />
 				<img src={certificate1} id="certificatepage-certificate1" />
 				<div>
 					<text className="text1-certificatepage"> Certificado do curso "Como Criar seu Currículo"</text>
